b892d63ac3870288915e409834c378ee28835e95
MariaHelena1B <108529028+MariaHelena1B@users.noreply.github.com>
2024-04-19 08:28:41 -0300

    7,6

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
-const tempoObjetivo2 = new Date("2024-03-27T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-27T00:00:00");
 const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-06-12T00:00:00");
 
